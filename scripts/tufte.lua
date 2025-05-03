387ae95c8860e7fa2551059ba2a49ec61f6ecf95
Dave Bridges <dave.bridges@gmail.com>
2025-05-03 18:23:57 -0400

    Added lua script for sidenotes/marginnotes/marginfigures

---
+++ scripts/tufte.lua
@@ -0,0 +1,9 @@
+# Molecular Nutrition
+
+## License
+
+[![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)
+
+This repository and its contents are licensed under the
+[Creative Commons Attribution 4.0 International License](https://creativecommons.org/licenses/by/4.0/).
+Attribution: Dave Bridges.
